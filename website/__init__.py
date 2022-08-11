77bcf68221d74c139451cd67464ec62e89447104
piotr <piotrekzrydek@gmial.com>
2022-08-11 21:10:41 +0200

    Bar chart for 24h water consumption

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from os import path
 import mysql.connector
 
+
+
 host="178.79.191.194"
 user="maczo_all"
 passwd="Pomidor123!@#"
@@ -27,3 +29,48 @@ def create_app():
     return app
 
 
+
+
+
+
+
+#==================== functions ==========================
+
+
+def select_value_db(select_what,select_from,select_howmany,):
+    
+    mycursor = db.cursor()
+
+    mycursor.execute(f"SELECT {select_what} FROM {select_from} ORDER BY id DESC LIMIT {select_howmany}")
+
+    resoult = mycursor.fetchall()
+
+    test_list = []
+    value_list = []
+
+    for i in resoult:
+        test_list.append(i)
+        
+
+    for i in range(0,len(test_list)):
+        value= list(f"{test_list[i]}")
+
+        del value[0:1]
+        del value[-2:]
+        
+        joined = "".join(value)
+
+        value_list.append(joined)
+
+
+    return value_list
+
+
+def water_use_per_hour(hours):
+    x = hours * 60
+    z = x/5
+    z = int(z)
+
+    chuj = select_value_db("value","water_use",z)
+    wynik = float(chuj[0])- float(chuj[-1])
+    return wynik
--- website/sites.py
+++ website/sites.py
@@ -1,16 +1,28 @@
 from flask import Blueprint, render_template
-
-
+from . import *
+import pygal
 views = Blueprint('views', __name__)
+
+
+
 
 
 
 @views.route('/')
 def home():
 
-    return render_template('home.html')
+    '''Renders bar graph to web page based on Fibonacci sequence.'''
+    bar_chart = pygal.Bar(height=300)  # instance of Bar class
+    bar_chart.title = 'Zużycie wody przez 7h'  # title of bar chart
+    bar_chart.add('litry', [water_use_per_hour(24), water_use_per_hour(20), water_use_per_hour(10), water_use_per_hour(6), water_use_per_hour(4), water_use_per_hour(2), water_use_per_hour(1)])  # add fibo data to chart
+    chart = bar_chart.render_data_uri()  # render bar chart
+
+
+    return render_template('home.html',chart=chart)
 
 
 @views.route('/login')
 def login():
     return render_template('login.html')
+
+
